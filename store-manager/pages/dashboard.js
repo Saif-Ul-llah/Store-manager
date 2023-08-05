2392ff4f0a21f6e5f8ece903d70c0fffccb58816
Saif-Ul-llah <102151531+Saif-Ul-llah@users.noreply.github.com>
2023-08-05 23:41:27 +0500

    5/8

---
+++ store-manager/pages/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideNavbar from '../components/sideNavbar'
+
+const Dashboard = () => {
+  return (
+    <>
+    <div>dashboard</div>
+    <SideNavbar/>
+    </>
+  )
+}
+
+export default Dashboard
